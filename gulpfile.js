6befab3b334f90b2a6c6ee19bc70380765699975
Foxside <47948806+Foxside@users.noreply.github.com>
2019-07-02 20:32:08 +0500

    update v2.0
    + JS converting with Babel
    + JS minifyig with UglifyJS
    + better folder structuring

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,13 @@ const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
 
 function serve() {
-    browserSync.init({server: './'});
+    browserSync.init({server: './', browser: 'chrome', notify: false});
     watch('./sass/*.sass', sassCompiling);
+    watch('./js/*.js', jsCompiling);
     watch('./*.html').on('change', browserSync.reload);
 }
 
@@ -17,7 +20,16 @@ function sassCompiling() {
         .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
         .pipe(cssnano())
         .pipe(rename({suffix: '.min'}))
-        .pipe(dest('./dist'))
+        .pipe(dest('./dist/css'))
+        .pipe(browserSync.stream());
+}
+
+function jsCompiling() {
+    return src('./js/*.js')
+        .pipe(babel({presets: ['@babel/env']}))
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(dest('./dist/js'))
         .pipe(browserSync.stream());
 }
 
